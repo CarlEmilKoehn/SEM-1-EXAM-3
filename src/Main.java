93fd8c54935af838cf43344844c57414fc4f5b3d
CarlEmilKoehn <carledk@live.dk>
2025-06-01 16:21:48 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -12,17 +12,24 @@ public class Main {
         names.add("Stine");
 
         //loading the text file into a string
+        try {
         String text = "";
-        File file = new File("doc/personal_info_list.txt");
-        Scanner sc = new Scanner(file);
-        while (sc.hasNextLine()) {
-            text += sc.nextLine() + "\n";
+
+            File file = new File("data/personal_info_list.txt");
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                text += sc.nextLine() + "\n";
+            }
+            sc.close();
+
+            //Instantiating stringhandler to replace text
+            StringHandler stringHandler = new StringHandler();
+            String newText = stringHandler.replaceText(text, names);
+            //Printing the anonymised text
+            System.out.println(newText);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found" + e.getMessage());
         }
-        sc.close();
 
-        StringHandler stringHandler = new StringHandler();
-        String newText = stringHandler.replaceText(text, names);
-
-        System.out.println(newText);
     }
 }
